97b5676cfc1d4861777c5690bc028cc8b1aa60d7
WildSteph <Stephaine.Pantiez@gmail.com>
2020-05-21 17:08:45 +0200

    refactor(app.jsx) : build router '

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import Multidice from './component/Multidices'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Multidice from './component/Multidices';
 import './App.css';
+import Principal from './component/Principal';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Multidice />
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dices">Dices</Link>
+            </li>
+            <li>
+              <Link to="/character">Character</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dices">
+            <Multidice />
+          </Route>
+          <Route path="/character">
+            <Principal />
+          </Route>
+          <Route path="/">
+            <p>home</p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>  
       </header>
     </div>
   );
