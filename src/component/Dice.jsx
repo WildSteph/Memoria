af0582f4dc60528eb96f5fd8f1825430880f25b7
WildSteph <Stephaine.Pantiez@gmail.com>
2020-05-21 17:06:12 +0200

    refactor(dice.jsx): bluit props

--- src/component/Dice.jsx
+++ src/component/Dice.jsx
@@ -7,7 +7,8 @@ function Dice(props) {
 
   return (
     <div>
-      <p> {(Math.floor((Math.random() * props.maxDiceValue) + 1))}</p>
+      <p> {(Math.floor((Math.random() * props.maxDiceValue) + 1))}
+      </p>
     </div>
   );
 }
