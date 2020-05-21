5322b88845c70fbff80b8e30b7222948db02a5a4
WildSteph <Stephaine.Pantiez@gmail.com>
2020-05-21 17:06:53 +0200

    refactor(Multidice.jsx): bluit props

--- src/component/Multidices.jsx
+++ src/component/Multidices.jsx
@@ -5,23 +5,30 @@ import Dice from './Dice'
 function Multidices() {
     const [curentMaxDiceValue, setCurentMaxDiceValue] = useState(0);
     const [multiple, setMultiple] = useState(0)
-    const[print, setPrint]= useState(false)
-
+    const [curentMultiple, SetCurentMultiple] = useState(0)
+    const [curentDiceValue, setCurentDiceValue] = useState(0)
+    const [print, setPrint] = useState(false)
     var rows = [];
-    for (let i = 0; i < multiple; i++) {
-        rows.push(<Dice maxDiceValue={curentMaxDiceValue} key={i} />);
+    for (let i = 0; i < curentMultiple; i++) {
+        rows.push(<Dice maxDiceValue={curentDiceValue} key={i} />);
     }
-    return (
-        <div>
-            <input type="number" onChange={(event) => setMultiple(event.target.value)} />
-            <input type="number" onChange={(event) => setCurentMaxDiceValue(event.target.value)} />
-            <button onClick={() => setPrint(true)}>set</button>
-            {print && (
-                <div>{rows}</div>
-            )}
-            
-        </div>
-    );
+        var getstate= () => {
+            setPrint(true)
+            SetCurentMultiple(multiple)
+            setCurentDiceValue(curentMaxDiceValue)
+        }
+        
+        return (
+            <div>
+                <input type="number" onChange={(event) => setMultiple(event.target.value)} />
+                <input type="number" onChange={(event) => setCurentMaxDiceValue(event.target.value)} />
+                <button onClick={() => getstate()}>set</button>
+                {print && (
+                    <div>{rows}</div>
+                )}
+
+            </div>
+        )
 }
 
 export default Multidices
